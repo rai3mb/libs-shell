e980d24e10b9cc17fef281f0101bda7ec1cc9758
portela <rai3mb@gmail.com>
2017-01-11 22:53:02 -0200

    nova mudanca

--- cut-video.sh
+++ cut-video.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
-# cut video / split video 
+# cut video / split video
 # need file and start/end part
 #==================================
+source /opt/scripts/libs/util.lib || exit
+_checkDependences 'ffmpeg'
 VIDEO="$1"
-[ -f "$VIDEO" ] || exit 1
+_file "$VIDEO"
 START="$2"
 END="$3"
 QTD=$(( $(echo $VIDEO | tr '.' '\n' | wc -l) -1 ))
-ffmpeg -i "$VIDEO" -vcodec copy -acodec copy -ss $START -t $END "$( echo $VIDEO | cut -d'.' -f1-${QTD} )"_part0${4:-1}.${VIDEO/*.}
-
+ffmpeg -i "$VIDEO" -vcodec copy -acodec copy -ss $START -t $END \
+    "$( echo $VIDEO | cut -d'.' -f1-${QTD} )"_part0${4:-1}.${VIDEO/*.}
